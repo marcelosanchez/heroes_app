333da71b076ed0fdd140e269a4bb3c2377b15824
marcelosanchez <msanchez@comextweb.com>
2022-05-31 22:47:04 -0500

    Domain updated

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/heroes_app/precache-manifest.496cd4c84deac6f9df647f745d50926f.js"
+  "/precache-manifest.410a7b44f53acdac2f1346c1a675bd86.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/heroes_app/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
